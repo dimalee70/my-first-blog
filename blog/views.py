c27a302db70eac467860116fd185e02b878a1fef
dimas <dimalee70@gmail.com>
2016-08-10 16:58:56 +0600

    Add the draft list

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,15 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
 def post_comment_list(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_comment_list.html', {'post': post})
